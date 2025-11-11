98e0e273e36f08ee1be5ab26decb14695370c1ba
Rizky Hanifa Afania <rizkyhanifa33@gmail.com>
2025-11-12 01:27:26 +0700

    fix: update footer text color for improved visibility

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="py-4 mt-4  border-gray-200">
-      <p className="text-center text-gray-500 text-sm font-semibold">
+      <p className="text-center text-gray-700 text-sm font-semibold">
         © 2025 Conversify all rights reserved
       </p>
     </footer>
